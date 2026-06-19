a2a21b593eee65ab006ba33a198dc86d929ad3fb
Muhammad Medhat <1615065@stemegypt.edu.eg>
2026-06-19 19:04:06 +0300

    Edit a new user credential for registration

--- src/main/java/TestData/TestData.java
+++ src/main/java/TestData/TestData.java
@@ -1,7 +1,7 @@
 package TestData;
 
 public class TestData {
-    public static final String USERNAME = "Test.User";
+    public static final String USERNAME = "Test.User2";
     public static final String PASSWORD = "Test123!";
     public static final String EMAIL = USERNAME + "@test.com";
 
