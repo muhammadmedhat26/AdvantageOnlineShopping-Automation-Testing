5bdfb6aff89ebe95fb7209dd8c3ae7d018df8236
Muhammad Medhat <1615065@stemegypt.edu.eg>
2026-06-19 18:58:55 +0300

    Finalize Homepage

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,4 +1,79 @@
 package pages;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class HomePage extends BasePage {
+
+    private By userIcon = By.id("menuUser");
+    private By createAccountLink = By.xpath("//a[contains(text(),'CREATE NEW ACCOUNT')]");
+    private By speakersCategory = By.id("speakersTxt");
+    private By cartIcon = By.id("menuCart");
+    private By loginButton = By.id("sign_in_btn");
+    private By myAccountLabel = By.xpath("//label[contains(text(),'My account')]");
+    private By deleteAccountButton = By.xpath("//button[contains(text(),'Delete Account') or contains(@translate,'DELETE_ACCOUNT')]");
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickUserIcon() {
+        System.out.println("Clicking user icon");
+        waitForLoaderToDisappear();
+        driver.findElement(userIcon).click();
+    }
+
+    public void waitForLoginPopup() {
+        System.out.println("Waiting for login popup to appear");
+        waitForLoaderToDisappear();
+        new WebDriverWait(driver, Duration.ofSeconds(20))
+                .until(d -> driver.findElement(loginButton).isDisplayed() && driver.findElement(createAccountLink).isDisplayed());
+    }
+
+    public void clickCreateAccount() {
+        System.out.println("Clicking create account link");
+        waitForLoaderToDisappear();
+        driver.findElement(createAccountLink).click();
+    }
+
+    public void clickSpeakersCategory() {
+        System.out.println("Clicking speakers category");
+        waitForLoaderToDisappear();
+        driver.findElement(speakersCategory).click();
+    }
+
+    public void clickCartIcon() {
+        System.out.println("Clicking cart icon");
+        waitForLoaderToDisappear();
+        driver.findElement(cartIcon).click();
+    }
+
+    public void navigateToMyAccount() {
+        System.out.println("Clicking My Account");
+        waitForLoaderToDisappear();
+        driver.findElement(userIcon).click();
+        driver.findElement(myAccountLabel).click();
+    }
+
+    public void clickDeleteAccount() {
+        waitForLoaderToDisappear();
+        driver.findElement(deleteAccountButton).click();
+    }
+
+    public boolean isUserLoggedIn(String username) {
+        waitForLoaderToDisappear();
+        try {
+            WebElement userElement = new WebDriverWait(driver, Duration.ofSeconds(10))
+                    .until(ExpectedConditions.visibilityOfElementLocated(
+                            By.xpath("//*[@id='menuUserLink']/span[contains(.,'" + username + "')]")
+                    ));
+            return userElement.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
